581b0c5117d390cb3b30bbbb13cee7c9a5fa0ee0
Marc Maetz <mmaetz@student.ethz.ch>
2014-12-26 18:05:48 +0100

    init ex 2 task 3

---
+++ ex2/task3.cpp
@@ -0,0 +1,91 @@
+#include "latticeview.h"
+#include <stdlib.h>
+
+#define N 50             //Lateral number of cells
+#define ImageWidth 500  //image width
+#define ImageHeight 500 //image height
+
+
+using namespace std;
+
+main ()
+{
+  int lat[N*N];  //create lattice
+	int temp_lat[N*N];
+  int icounter, jcounter; //counters
+	double p = 0.5;
+	std::mt19937 gen(42);
+	std::uniform_real_distribution<double> dis(0,1);
+
+  for (icounter=0; icounter<N*N; icounter++) lat[icounter]=0;
+		for (icounter=0; icounter<N*N; icounter++)
+		{
+			if(dis(gen)<p)
+				lat[icounter]= 1;
+		}
+  Print_lattice (lat, N, N, ImageWidth, ImageHeight, "task2-1.ppm");
+	for (icounter = 0; icounter<N; icounter++)
+	{
+		if(lat[icounter] == 1)
+			lat[icounter] = 2;
+	}
+
+	bool burn=1;
+	bool reach=0;
+	int giter = 0;
+	while(burn)
+	{
+		burn=0;
+		for (icounter = 0; icounter < N*N; icounter++)
+			if(lat[icounter] == 2)
+			{
+				if(icounter > 0 && lat[icounter - 1] == 1)
+				{
+					lat[icounter - 1] = 4;
+					burn=1;
+				}
+				if(icounter < N*N && lat[icounter + 1] == 1)
+				{
+					lat[icounter + 1] = 4;
+					burn=1;
+				}
+				if(icounter<N*N-N && lat[icounter + N] == 1)
+				{
+					lat[icounter+N] = 4;
+					burn=1;
+				}
+				if(icounter > N && lat[icounter - N] == 1)
+				{
+					lat[icounter-N] = 4;
+					burn=1;
+				}
+			}
+
+		for (icounter = 0; icounter < N*N; icounter++)
+		{
+			if(lat[icounter] == 2)
+				lat[icounter] = 3;
+			if(lat[icounter] == 4)
+				lat[icounter] = 2;
+		}
+		giter++;
+		for(icounter = N*N-N; icounter < N*N; icounter++)
+		{
+			if(lat[icounter]==2)
+			{
+				burn=0;
+				reach=1;
+				cout << "The fire does the reach other end and the number of steps was:" << endl;
+				cout << giter << endl;
+			}
+		}
+	}
+	if(!reach)
+	{
+		cout << "The fire does not reach the other end and the number of steps was:" << endl;
+		cout << giter << endl;
+	}
+
+  Print_lattice (lat, N, N, ImageWidth, ImageHeight, "task2-2.ppm");
+
+}
